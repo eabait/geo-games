9e34d7d80ff53b2f40213178ad5a7ac5c8303d3c
eabait <estebanabait@gmail.com>
2026-04-23 10:40:35 -0300

    feat: add global styling foundation

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { router } from './router/index';
+import './styles/index.css';
 
 const queryClient = new QueryClient();
 
